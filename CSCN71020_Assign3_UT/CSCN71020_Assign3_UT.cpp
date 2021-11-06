3145b516ed398386073b482ccbb92e33da76b640
Mostafa Elbasiouny <Melbasiouny4793@conestogac.on.ca>
2021-11-06 13:38:53 -0400

    Add and setup unit testing.

---
+++ CSCN71020_Assign3_UT/CSCN71020_Assign3_UT.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CSCN71020Assign3UT
+{
+	TEST_CLASS(CSCN71020Assign3UT)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
