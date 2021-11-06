18c9fbd61e566d90017432a8934bd4a5ffa50ce4
Mostafa Elbasiouny <Melbasiouny4793@conestogac.on.ca>
2021-11-06 14:23:28 -0400

    Add more test cases.

--- CSCN71020_Assign3_UT/CSCN71020_Assign3_UT.cpp
+++ CSCN71020_Assign3_UT/CSCN71020_Assign3_UT.cpp
@@ -28,5 +28,59 @@ namespace CSCN71020Assign3UT
 			char* winner = RPS("Stick", "Paper");
 			Assert::AreEqual("Invalid", winner);
 		}
+
+		// Test-Case:	Checks when Player1 chooses Rock and 
+		//				Player2 chooses Scissors the winner is Player1.
+		// Output:		Returns "Player1".
+		TEST_METHOD(RPS_Player1_RockVSScissors)
+		{
+			char* winner = RPS("Rock", "Scissors");
+			Assert::AreEqual("Player1", winner);
+		}
+
+		// Test-Case:	Checks when Player1 chooses Paper and 
+		//				Player2 chooses Rock the winner is Player1.
+		// Output:		Returns "Player1".
+		TEST_METHOD(RPS_Player1_PaperVSRock)
+		{
+			char* winner = RPS("Paper", "Rock");
+			Assert::AreEqual("Player1", winner);
+		}
+
+		// Test-Case:	Checks when Player1 chooses Scissors and 
+		//				Player2 chooses Paper the winner is Player1.
+		// Output:		Returns "Player1".
+		TEST_METHOD(RPS_Player1_ScissorsVSPaper)
+		{
+			char* winner = RPS("Scissors", "Paper");
+			Assert::AreEqual("Player1", winner);
+		}
+
+		// Test-Case:	Checks when Player1 chooses Scissors and 
+		//				Player2 chooses Rock the winner is Player1.
+		// Output:		Returns "Player2".
+		TEST_METHOD(RPS_Player2_ScissorsVSRock)
+		{
+			char* winner = RPS("Scissors", "Rock");
+			Assert::AreEqual("Player2", winner);
+		}
+
+		// Test-Case:	Checks when Player1 chooses Rock and 
+		//				Player2 chooses Paper the winner is Player1.
+		// Output:		Returns "Player2".
+		TEST_METHOD(RPS_Player2_RockVSPaper)
+		{
+			char* winner = RPS("Rock", "Paper");
+			Assert::AreEqual("Player2", winner);
+		}
+
+		// Test-Case:	Checks when Player1 chooses Paper and 
+		//				Player2 chooses Scissors the winner is Player1.
+		// Output:		Returns "Player2".
+		TEST_METHOD(RPS_Player2_PaperVSScissors)
+		{
+			char* winner = RPS("Paper", "Scissors");
+			Assert::AreEqual("Player2", winner);
+		}
 	};
 }
