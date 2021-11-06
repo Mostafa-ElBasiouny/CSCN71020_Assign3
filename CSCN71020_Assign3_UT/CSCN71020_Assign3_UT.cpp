a3c1308873e324e8484ba374648bd1d0282c767f
Mostafa Elbasiouny <Melbasiouny4793@conestogac.on.ca>
2021-11-06 14:06:13 -0400

    Add test cases.

--- CSCN71020_Assign3_UT/CSCN71020_Assign3_UT.cpp
+++ CSCN71020_Assign3_UT/CSCN71020_Assign3_UT.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" char* RPS(char* playerOne, char* playerTwo);
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace CSCN71020Assign3UT
@@ -8,9 +10,23 @@ namespace CSCN71020Assign3UT
 	TEST_CLASS(CSCN71020Assign3UT)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		// All test methods uses the following naming convention:
+		// [FUNCTION-UNDER-TEST]_[FUNCTION-OUTPUT]_[TEST-CASE]
+
+		// Test-Case:	Checks if both players had the same choice.
+		// Output:		Returns "Draw".
+		TEST_METHOD(RPS_Draw_WhenBothInputsAreEqual)
 		{
+			char* winner = RPS("Rock", "Rock");
+			Assert::AreEqual("Draw", winner);
+		}
+
+		// Test-Case:	Checks if any choice entered is invalid.
+		// Output:		Returns "Invalid".
+		TEST_METHOD(RPS_Invalid_WhenInputIsInvalid)
+		{
+			char* winner = RPS("Stick", "Paper");
+			Assert::AreEqual("Invalid", winner);
 		}
 	};
 }
