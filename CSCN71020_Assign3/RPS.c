fad7673ac9e02a6686d76b64429b0504a1629535
Mostafa Elbasiouny <Melbasiouny4793@conestogac.on.ca>
2021-11-06 14:31:54 -0400

    Implement `RPS`.

--- CSCN71020_Assign3/RPS.c
+++ CSCN71020_Assign3/RPS.c
@@ -1,3 +1,15 @@
 void main(void) {}
 
-char* RPS(char* playerOne, char* playerTwo) {}
+char* RPS(char* playerOne, char* playerTwo) {
+	char* winner = "Invalid";
+	if (playerOne == playerTwo)
+		winner = "Draw";
+	else if (playerOne == "Rock")
+		winner = (playerTwo == "Scissors" ? "Player1" : "Player2");
+	else if (playerOne == "Paper")
+		winner = (playerTwo == "Rock" ? "Player1" : "Player2");
+	else if (playerOne == "Scissors")
+		winner = (playerTwo == "Paper" ? "Player1" : "Player2");
+
+	return winner;
+}
--- CSCN71020_Assign3_UT/CSCN71020_Assign3_UT.cpp
+++ CSCN71020_Assign3_UT/CSCN71020_Assign3_UT.cpp
@@ -14,7 +14,7 @@ namespace CSCN71020Assign3UT
 		// [FUNCTION-UNDER-TEST]_[FUNCTION-OUTPUT]_[TEST-CASE]
 
 		// Test-Case:	Checks if both players had the same choice.
-		// Output:		Returns "Draw".
+		// Output:	Returns "Draw".
 		TEST_METHOD(RPS_Draw_WhenBothInputsAreEqual)
 		{
 			char* winner = RPS("Rock", "Rock");
@@ -22,7 +22,7 @@ namespace CSCN71020Assign3UT
 		}
 
 		// Test-Case:	Checks if any choice entered is invalid.
-		// Output:		Returns "Invalid".
+		// Output:	Returns "Invalid".
 		TEST_METHOD(RPS_Invalid_WhenInputIsInvalid)
 		{
 			char* winner = RPS("Stick", "Paper");
@@ -30,8 +30,8 @@ namespace CSCN71020Assign3UT
 		}
 
 		// Test-Case:	Checks when Player1 chooses Rock and 
-		//				Player2 chooses Scissors the winner is Player1.
-		// Output:		Returns "Player1".
+		//		Player2 chooses Scissors the winner is Player1.
+		// Output:	Returns "Player1".
 		TEST_METHOD(RPS_Player1_RockVSScissors)
 		{
 			char* winner = RPS("Rock", "Scissors");
@@ -39,8 +39,8 @@ namespace CSCN71020Assign3UT
 		}
 
 		// Test-Case:	Checks when Player1 chooses Paper and 
-		//				Player2 chooses Rock the winner is Player1.
-		// Output:		Returns "Player1".
+		//		Player2 chooses Rock the winner is Player1.
+		// Output:	Returns "Player1".
 		TEST_METHOD(RPS_Player1_PaperVSRock)
 		{
 			char* winner = RPS("Paper", "Rock");
@@ -48,8 +48,8 @@ namespace CSCN71020Assign3UT
 		}
 
 		// Test-Case:	Checks when Player1 chooses Scissors and 
-		//				Player2 chooses Paper the winner is Player1.
-		// Output:		Returns "Player1".
+		//		Player2 chooses Paper the winner is Player1.
+		// Output:	Returns "Player1".
 		TEST_METHOD(RPS_Player1_ScissorsVSPaper)
 		{
 			char* winner = RPS("Scissors", "Paper");
@@ -57,8 +57,8 @@ namespace CSCN71020Assign3UT
 		}
 
 		// Test-Case:	Checks when Player1 chooses Scissors and 
-		//				Player2 chooses Rock the winner is Player1.
-		// Output:		Returns "Player2".
+		//		Player2 chooses Rock the winner is Player1.
+		// Output:	Returns "Player2".
 		TEST_METHOD(RPS_Player2_ScissorsVSRock)
 		{
 			char* winner = RPS("Scissors", "Rock");
@@ -66,8 +66,8 @@ namespace CSCN71020Assign3UT
 		}
 
 		// Test-Case:	Checks when Player1 chooses Rock and 
-		//				Player2 chooses Paper the winner is Player1.
-		// Output:		Returns "Player2".
+		//		Player2 chooses Paper the winner is Player1.
+		// Output:	Returns "Player2".
 		TEST_METHOD(RPS_Player2_RockVSPaper)
 		{
 			char* winner = RPS("Rock", "Paper");
@@ -75,8 +75,8 @@ namespace CSCN71020Assign3UT
 		}
 
 		// Test-Case:	Checks when Player1 chooses Paper and 
-		//				Player2 chooses Scissors the winner is Player1.
-		// Output:		Returns "Player2".
+		//		Player2 chooses Scissors the winner is Player1.
+		// Output:	Returns "Player2".
 		TEST_METHOD(RPS_Player2_PaperVSScissors)
 		{
 			char* winner = RPS("Paper", "Scissors");
